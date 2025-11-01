67114942498ec2ebb086de2ee10d131c1e4c73d9
George <george.arutinyan@gmail.com>
2025-11-02 00:21:42 +0400

    ready for deployment

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+import authRoutes from './routes/auth.route';
+import messageRoutes from './routes/message.route';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+  app.get(/^(?!\/api).*/, (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
